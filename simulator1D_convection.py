3222df3d553e3f4f9127f2ed53cef08273af71a2
Leandro Marques <marquesleandro67@gmail.com>
2019-04-12 17:27:01 -0300

    modification

--- simulator1D_convection.py
+++ simulator1D_convection.py
@@ -50,18 +50,27 @@ Re = float(raw_input(" Enter Reynolds Number (Re): "))
 Sc = float(raw_input(" Enter Schmidt Number (Sc): "))
 print ""
 
+print ' (1) - Linear Element'
+print ' (2) - Quadratic Element'
+print ' (3) - Cubic Element'
+polynomial_option = int(raw_input(" Enter polynomial degree option above: "))
+print ""
+
+print ' 3 Gauss Points'
+print ' 4 Gauss Points'
+print ' 5 Gauss Points'
+gausspoints = int(raw_input(" Enter Gauss Points Number option above: "))
+print ""
+
 nt = int(raw_input(" Enter number of time interations (nt): "))
 directory_name = raw_input(" Enter folder name to save simulations: ")
 
 print ""
 print ' (1) - Taylor Galerkin'
-print ' (2) - Semi Lagrangian'
+print ' (2) - Semi Lagrangian Linear'
+print ' (3) - Semi Lagrangian Quadratic'
 scheme_option = int(raw_input(" Enter simulation scheme option above: "))
 print ""
-
-# INCLUIR O INPUT DO SIMULATOR2D
-polynomial_order = 'teste'
-gausspoints = 1
 
 
 print ' ------------'
@@ -74,9 +83,15 @@ directory = search_file.Find(mesh_name)
 if directory == 'File not found':
  sys.exit()
 
-mesh = import_msh.Linear1D(directory,mesh_name,equation_number)
-mesh.coord()
-mesh.ien()
+if polynomial_option == 1:
+ mesh = import_msh.Linear1D(directory,mesh_name,equation_number)
+ mesh.coord()
+ mesh.ien()
+
+elif polynomial_option == 2:
+ mesh = import_msh.Quad1D(directory,mesh_name,equation_number)
+ mesh.coord()
+ mesh.ien()
 
 CFL = 0.5
 dt = float(CFL*mesh.length_min)
@@ -95,7 +110,7 @@ print ' ---------'
 
 start_time = time()
 
-K, M, G = assembly.Linear1D(mesh.GL, mesh.npoints, mesh.nelem, mesh.IEN, mesh.x)
+K, M, G, polynomial_order = assembly.Element1D(polynomial_option, mesh.GL, mesh.npoints, mesh.nelem, mesh.IEN, mesh.x, gausspoints)
 
 LHS_c0 = (sps.lil_matrix.copy(M)/dt)
 
@@ -159,58 +174,64 @@ print ""
 print ' Saving simulation in %s' %directory_name
 print ""
 
-start_time = time()
-for t in tqdm(range(0, nt)):
- 
- # ------------------------ Export VTK File --------------------------------------
- save = export_vtk.Linear1D(mesh.x,mesh.IEN,mesh.npoints,mesh.nelem,c,c,c,vx,vx)
- save.create_dir(directory_name)
- save.saveVTK(directory_name + str(t))
- # -------------------------------------------------------------------------------
 
- # -------------------------------- Solver ---------------------------------------
- # Taylor Galerkin
- if scheme_option == 1:
+
+# Taylor Galerkin
+if scheme_option == 1:
+ 
+ start_time = time()
+ for t in tqdm(range(0, nt)):
+ 
+  # ------------------------ Export VTK File --------------------------------------
+  save = export_vtk.Linear1D(mesh.x,mesh.IEN,mesh.npoints,mesh.nelem,c,c,c,vx,vx)
+  save.create_dir(directory_name)
+  save.saveVTK(directory_name + str(t))
+  # -------------------------------------------------------------------------------
+ 
+  # -------------------------------- Solver ---------------------------------------
   scheme = solver.SemiImplicit_convection_diffusion1D(scheme_option)
   scheme.taylor_galerkin(c, vx, dt, M, K, G, condition_concentration.LHS, condition_concentration.bc_dirichlet, condition_concentration.bc_2)
   c = scheme.c
+  # -------------------------------------------------------------------------------
 
- # Semi Lagrangian
- elif scheme_option == 2:
+
+# Semi Lagrangian Linear
+elif scheme_option == 2:
+
+ start_time = time()
+ for t in tqdm(range(0, nt)):
+ 
+  # ------------------------ Export VTK File --------------------------------------
+  save = export_vtk.Linear1D(mesh.x,mesh.IEN,mesh.npoints,mesh.nelem,c,c,c,vx,vx)
+  save.create_dir(directory_name)
+  save.saveVTK(directory_name + str(t))
+  # -------------------------------------------------------------------------------
+
+  # -------------------------------- Solver ---------------------------------------
   scheme = solver.SemiImplicit_convection_diffusion1D(scheme_option)
-  scheme.semi_lagrangian(mesh.npoints, mesh.neighbors_elements, mesh.IEN, mesh.x, mesh.y, vx, vy, dt, c, M, condition_concentration.LHS, condition_concentration.bc_dirichlet, condition_concentration.bc_2)
+  scheme.semi_lagrangian(mesh.npoints, mesh.neighbors_elements, mesh.IEN, mesh.x, vx, dt, c, M, condition_concentration.LHS, condition_concentration.bc_dirichlet, condition_concentration.bc_2)
   c = scheme.c
- # -------------------------------------------------------------------------------
+  # -------------------------------------------------------------------------------
+
+# Semi Lagrangian Quadratic
+elif scheme_option == 3:
+
+ start_time = time()
+ for t in tqdm(range(0, nt)):
+ 
+  # ------------------------ Export VTK File --------------------------------------
+  save = export_vtk.Linear1D(mesh.x,mesh.IEN,mesh.npoints,mesh.nelem,c,c,c,vx,vx)
+  save.create_dir(directory_name)
+  save.saveVTK(directory_name + str(t))
+  # -------------------------------------------------------------------------------
+ 
+  # -------------------------------- Solver ---------------------------------------
+  scheme = solver.SemiImplicit_convection_diffusion1D(scheme_option)
+  scheme.semi_lagrangian_quad(mesh.npoints, mesh.nelem, mesh.neighbors_elements, mesh.IEN, mesh.x, vx, dt, c, M, condition_concentration.LHS, condition_concentration.bc_dirichlet, condition_concentration.bc_2)
+  c = scheme.c
+  # -------------------------------------------------------------------------------
 
 
-
- # ----------------------- Solver - Taylor Galerkin ------------------------------
-# scheme = 'Taylor Galerkin'
-# A = np.copy(M)/dt
-# concentration_RHS = sps.lil_matrix.dot(A,c) - np.multiply(vx,sps.lil_matrix.dot(G,c))\
-#                   - (dt/2.0)*np.multiply(vx,(np.multiply(vx,sps.lil_matrix.dot(K,c))))
-# 
-# concentration_RHS = np.multiply(concentration_RHS,condition_concentration.bc_2)
-# concentration_RHS = concentration_RHS + condition_concentration.bc_dirichlet
-# 
-# c = scipy.sparse.linalg.cg(condition_concentration.LHS,concentration_RHS,c, maxiter=1.0e+05, tol=1.0e-05)
-# c = c[0].reshape((len(c[0]),1))
- # -------------------------------------------------------------------------------
-
-
- # ------------------------ Solver - Semi Lagrangian -----------------------------
-# scheme = 'Semi Lagrangian'
-# c_d = semi_lagrangian.Linear1D(mesh.npoints, mesh.neighbors_elements, mesh.IEN, mesh.x, vx, dt, c)
-#
-# A = np.copy(M)/dt
-# concentration_RHS = sps.lil_matrix.dot(A,c_d)
-# 
-# concentration_RHS = np.multiply(concentration_RHS,condition_concentration.bc_2)
-# concentration_RHS = concentration_RHS + condition_concentration.bc_dirichlet
-#
-# c = scipy.sparse.linalg.cg(condition_concentration.LHS,concentration_RHS,c, maxiter=1.0e+05, tol=1.0e-05)
-# c = c[0].reshape((len(c[0]),1))
- # -------------------------------------------------------------------------------
 
 end_time = time()
 solution_time = end_time - start_time
